0a3aa6cf9da171e422ca829bdf186c96e706b130
giraud <giraud.contact@yahoo.fr>
2018-02-08 22:12:32 +0100

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 rm -f *.cm* *.exe *.o
-#ocamlc -g -I +compiler-libs -o a.out ocamlcommon.cma cmt_annot.ml
 ocamlopt -I +compiler-libs -o rincewind.exe ocamlcommon.cmxa rincewind.ml
