ec1488a3df62996f26c8602392e9d2c1b3040741
giraud <giraud.contact@yahoo.fr>
2018-02-09 15:59:31 +0100

    test

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-ls ~/bin
-PATH=~/ocaml/bin:$PATH
+ls -la ~
+#PATH=~/ocaml/bin:$PATH
 
 rm -f *.cm* *.exe *.o
 ocamlopt -I +compiler-libs -o rincewind.exe ocamlcommon.cmxa str.cmxa rincewind.ml
