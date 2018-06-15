64b5df3bd33a5770a9c0139e215154516bc0f837
giraud <giraud.contact@yahoo.fr>
2018-06-15 12:08:37 +0200

    better format for I / remove col + 1

--- Formatter.ml
+++ Formatter.ml
@@ -8,10 +8,10 @@ let clean_type str =
   Str.global_replace r " " str
 
 let format_position pos =
-  (string_of_int pos.pos_lnum) ^ "." ^ (string_of_int (pos.pos_cnum - pos.pos_bol(*begining of line*) + 1))
+  (string_of_int pos.pos_lnum) ^ "." ^ (string_of_int (pos.pos_cnum - pos.pos_bol(*begining of line*)))
 
 let format_location {loc_start; loc_end; loc_ghost} =
-  format_position loc_start (*^ "," ^ (position_to_string loc_end)*)
+  format_position loc_start (*^ "," ^ (format_position loc_end)*)
 
 let format_resolved_item ~kind ~loc ~path ~name ~typ =
     let kind_name = string_of_entry_kind kind in
