a1284115c4cfd389223a2aeaa0f0626329e6087e
giraud <giraud.contact@yahoo.fr>
2018-06-15 17:37:33 +0200

    lines start at 0

--- Formatter.ml
+++ Formatter.ml
@@ -8,7 +8,7 @@ let clean_type str =
   Str.global_replace r " " str
 
 let format_position pos =
-  (string_of_int pos.pos_lnum) ^ "." ^ (string_of_int (pos.pos_cnum - pos.pos_bol(*begining of line*)))
+  (string_of_int (pos.pos_lnum - 1)) ^ "." ^ (string_of_int (pos.pos_cnum - pos.pos_bol(*begining of line*)))
 
 let format_location {loc_start; loc_end; loc_ghost} =
   format_position loc_start (*^ "," ^ (format_position loc_end)*)
