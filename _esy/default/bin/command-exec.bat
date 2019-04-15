af3625711caf756033ce40cb8d7180ba82b51212
giraud <giraud.contact@yahoo.fr>
2019-04-15 18:27:36 +0200

    use ci from hello-reason

---
+++ _esy/default/bin/command-exec.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+@SETLOCAL
+"C:\Users\herve\AppData\Roaming\npm\node_modules\esy\_build\default\bin\esy.exe" exec-command --include-npm-bin --include-current-env --include-build-env --project "C:\dev\src\rincewind" %*
---
+++ _esy/default/bin/node.cmd
@@ -0,0 +1,5 @@
+@ECHO off
+@SETLOCAL
+@SET ESY__NODE_BIN_PATH=%C:\dev\src\rincewind\_esy\default\bin%
+"C:\dev\nodejs\node.exe" -r "C:\dev\src\rincewind\_esy\default\pnp.js" %*
+            
